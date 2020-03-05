34537aa2ac433b5129eceb7cd2a40822b466d610
Mariott-Dev <mariottdesign@gmail.com>
2020-03-05 16:22:14 -0700

    Forecast Commit1

---
+++ Franks International/Pages/Index.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Franks_InternationalML.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.ML;
+
+
+namespace Franks_International.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly PredictionEnginePool<ModelInput, ModelOutput> _predictionEnginePool;
+
+        public IndexModel(PredictionEnginePool<ModelInput, ModelOutput> predictionEnginePool)
+        {
+            _predictionEnginePool = predictionEnginePool;
+        }
+
+        [BindProperty]
+        public float userYear { get; set; }
+        [BindProperty]
+        public DateTime userMonth { get; set; }
+        [BindProperty]
+        public int UserDuration { get; set; }
+
+        //scratch code below
+
+        //int[] arrayYear = new int[] { 2019, 2020, 2021 }; //array for input years
+        //int[] arrayMonth = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }; //array for input months
+        List<float> lPredictionScore = new List<float>(); // initialization of list for predictions
+        List<int> lPredictionMonth = new List<int>(); //initialization of list of months
+
+        //int[] arrayDuration = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+        //end scratch code
+
+        public void OnPost(int userYear, int userMonth, int userDuration, float userDayRevenue = 0)
+        {
+            for (int i = 0; i < userDuration; i++)
+            {
+                float userDay = DateTime.DaysInMonth(userYear, userMonth);
+                var input = new ModelInput { Year = userYear, Month = userMonth, Day = userDay, Day_Revenue = userDayRevenue };
+                var prediction = _predictionEnginePool.Predict(input);
+                ViewData["confirmation"] += $"{userMonth}, {userDay}, {userYear}, forecast is: ${prediction.Score}<br />"; //testing output
+                lPredictionMonth.Add(userMonth); //add predicted month to list
+                lPredictionScore.Add(prediction.Score); //add prediction score to list
+                userMonth++;
+                if (userMonth > 12)
+                {
+                    userMonth = 1;
+                    userYear++;
+                }
+            }
+        }
+    }
+}
---
+++ Franks International/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+function renderChart(data, labels) {
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Historical Data',
+                data: data,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            }]
+        },
+    });
+}
+
+$("#renderBtn").click(
+    function () {
+        data = [267035.92, 279144.06, 270418.97, 397370.5, 362751.8, 269145.09, 247442.57, 231280.43, 263462.06, 290873.21, 323884.38, 296369.43];
+        labels = ["August", "September", "October", "November", "December", "January", "February", "March", "April", "May", "June", "July"];
+        renderChart(data, labels);
+    }
+);
